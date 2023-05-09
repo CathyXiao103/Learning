23fb66c0f22a9b10cd684aab2828beda1a628878
CathyXiao103 <cxiao@barfoot.co.nz>
2023-05-09 17:54:16 +1200

    update unit test to a shape

--- ExcersiseOne/ExcersiseOneTests/AnimalsControllerTests.cs
+++ ExcersiseOne/ExcersiseOneTests/AnimalsControllerTests.cs
@@ -1,9 +1,4 @@
 ﻿using ExcersiseOne;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExcersiseOneTests;
 
@@ -12,11 +7,14 @@ public class AnimalsControllerTests
     [Test]
     public void AddAnimal_Test()
     {
+        //Arrange
         var animalController = new AnimalsController();
         var cow = new Animal(); 
         var cat = new Animal();
         cow.Name = "Mia";
         cat.Name = "Kitty";
+
+        //Act
         animalController.AddAnimal(cow);
         animalController.AddAnimal(cat);
 
@@ -25,7 +23,7 @@ public class AnimalsControllerTests
         {
             Console.WriteLine($"animal's name is {animal.Name}");
         }
-
+        //Assert
         Assert.AreEqual(2, animalController.GetAnimals().Count);
         Console.Write($"Animal number is {animalController.GetAnimals().Count}");
     }
@@ -33,12 +31,16 @@ public class AnimalsControllerTests
     [Test]
     public void RemoveAnimal_Test()
     {
+        //Arrange
         var animalController = new AnimalsController();
         var cow = new Animal();
+
+        //Act
         animalController.AddAnimal(cow);
         Console.Write($"Before remove, Animal number is {animalController.GetAnimals().Count}\n");
         animalController.RemoveAnimal(cow);
 
+        //Assert
         Assert.AreEqual(0, animalController.GetAnimals().Count);
         Console.Write($"After remove,Animal number is {animalController.GetAnimals().Count}");
     }
@@ -46,11 +48,16 @@ public class AnimalsControllerTests
     [Test]
     public void AnimalSpeak_Test()
     {
+        //Arrange
         var animalController = new AnimalsController();
         var cow = new Cow("Moo","Brown");
         cow.Name = "Mia";
+
+        //Act
         var tempSpeak = animalController.MakeAnimalSpeak(cow);
         Console.WriteLine($"{tempSpeak}");
+
+        //Assert
         Assert.AreEqual("Mia says Moo", tempSpeak);
       
     }
--- ExcersiseOne/ExcersiseOneTests/CowTests.cs
+++ ExcersiseOne/ExcersiseOneTests/CowTests.cs
@@ -13,20 +13,32 @@ public class CowTests
     [Test]
     public void Cow_Color_Test()
     {
+        //Arrange
         Cow cow = new Cow("Mia","Brown");
+
+        //Act
         cow.Name = "Mia";
-        var cowColor = "Mia is Brown";
-        Assert.AreEqual(cow.CowColour(), cowColor);
+        var CowColor = cow.CowColour();
+        var expectedCowColor = "Mia is Brown";
+
+        //Assert
+        Assert.AreEqual(expectedCowColor, CowColor);
 
     }
 
     [Test]
     public void Cow_Speak_Test()
     {
+        //Arrange
         Cow cow = new Cow("Moo", "Brown");
         cow.Name = "Mia";
-        var cowSound = "Mia says Moo";
-        Assert.That(cowSound,Is.EqualTo(cow.Speak()));
+
+        //Act
+        var expectedcowSound = "Mia says Moo";
+        var cowSpeak = cow.Speak();
+
+        //Assert
+        Assert.AreEqual(expectedcowSound,cowSpeak);
     }
         
 }
