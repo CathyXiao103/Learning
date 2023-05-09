ecd51ad9106c839bfdbc46707f2b351f5bf20874
CathyXiao103 <cxiao@barfoot.co.nz>
2023-05-09 18:01:02 +1200

    update unit test

--- ExcersiseTwo/ExcersiseTwoTest/CounterTest.cs
+++ ExcersiseTwo/ExcersiseTwoTest/CounterTest.cs
@@ -11,28 +11,43 @@ public class CounterTest
     [Test]
     public void Test_Tnitial_InstanceCountandTotalCount()
     {
-        Counter counter1 = new Counter(0);      
-        Assert.AreEqual(0, counter1.InstanceCount);
+        //Arrange
+        Counter counter1 = new Counter(0);
+
+        //Act
+        var initialCount = counter1.InstanceCount;
+
+        //Assert
+        Assert.AreEqual(0, initialCount);
         Assert.AreEqual(0, Counter.TotalCount);
     }
     [Test]
     public void Test_InstanceCountandTotalCount_AfterIncrement()
     {
+        //Arrange
         Counter counter1 = new Counter();
         Counter counter2 = new Counter();
 
-        Assert.AreEqual(1, counter1.InstanceCount);
-        Assert.AreEqual(1, counter2.InstanceCount);
-        Assert.AreEqual(2, Counter.TotalCount);
+        //Act
+        var intantCount1 = counter1.InstanceCount;
+        var intantCount2 = counter2.InstanceCount;
+        var totalCount = Counter.TotalCount;
+
+        //Assert
+        Assert.AreEqual(1, intantCount1);
+        Assert.AreEqual(1, intantCount2);
+        Assert.AreEqual(2, totalCount);
     }
     [Test]
     public void InstanceCountandTotalCount_AfterMultiIncrement()
     {
+        //Arrange&Act
         Counter counter1 = new Counter();
         Counter counter2 = new Counter();
         Counter counter3 = new Counter();
         Counter counter4 = new Counter();
 
+        //Assert
         Assert.AreEqual(1, counter1.InstanceCount);
         Assert.AreEqual(1,counter2.InstanceCount);
         Assert.AreEqual(1,counter3.InstanceCount);
