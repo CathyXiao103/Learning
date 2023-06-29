5a318bab146288208bacf4ef6e90d18f923df7c1
CathyXiao103 <cxiao@barfoot.co.nz>
2023-06-30 08:09:02 +1200

    e

--- ExcersiseSix/ExcersiseSix/UserPermissionService.cs
+++ ExcersiseSix/ExcersiseSix/UserPermissionService.cs
@@ -8,7 +8,7 @@ namespace ExcersiseSix;
 
 public class UserPermissionService : IUserPermissionService
 {
-    public bool UserHasAccess(string userId)
+    public bool UserHasAccess(string? userId)
     {
         if (userId == null)
             return false;
