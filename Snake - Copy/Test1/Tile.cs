a06ed7f66e3a5c8168b799cd5a66d0580c29fd55
CathyXiao103 <cxiao@barfoot.co.nz>
2024-05-23 16:33:01 +1200

    Create Tile.cs

---
+++ Snake - Copy/Test1/Tile.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadderGame
+{
+    public class Tile
+    {
+        public Tile()
+        {
+        }
+
+        public int PositionToTakeTo { get; set; }
+        public TileType TileType { get; set; }
+    }
+
+}
