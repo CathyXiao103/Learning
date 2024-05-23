21b13b103488275c1fbe013e93fee41b4c5c40c9
CathyXiao103 <cxiao@barfoot.co.nz>
2024-05-23 16:32:52 +1200

    Create Program.cs

---
+++ Snake - Copy/Test1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SnakeAndLadderGame;
+
+Console.WriteLine("Hello, World!");
+
+//Game Start and display welcome message.
+//Console.WriteLine("Welcome to Snake and Ladder Game!");
+//Console.WriteLine("Press any key to start the game.");
+//SnakeAndLadder game = new SnakeAndLadder();
+//game.Start();
+//Console.WriteLine(game.Start());
+//Console.ReadKey();
+
+////Roll the dice and get the dice number
+////Random random = new Random();
+//int diceNumber = game.DiceRoll();
+
+//Console.WriteLine("You rolled a dice and got: " + diceNumber);
